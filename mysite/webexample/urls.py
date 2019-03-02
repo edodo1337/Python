f4672fa8372bb09524dab26e8a6bacd9f886582d
edodo1337 <eldar100@Mail.ru>
2019-03-02 11:20:17 +0300

    2march

--- mysite/webexample/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import urls
-from . import views
-
-urlpatterns = [
-    path(r'^$', views.index, name = "index"),
-
-]
--- mysite/webexample/views.py
+++ mysite/webexample/views.py
@@ -1,11 +1,3 @@
 from django.shortcuts import render
-from django.Http import HttpResponse
-
-
-def index (request):
-    return(HttpResponse("<h3>Hello</h3">))
-
-
-
 
 # Create your views here.
