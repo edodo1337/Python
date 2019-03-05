e9d3d5a75b449d39ab1141870ded58ce05a2ea9a
edodo1337 <eldar100@Mail.ru>
2019-03-05 21:39:11 +0300

    5march

--- mysite/blog/forms.py
+++ mysite/blog/forms.py
@@ -4,4 +4,10 @@ from django import forms
 class TagForm(forms.Form):
     title = forms.CharField(max_length=50)
     slug = forms.CharField(max_length=50)
-    
+
+    def save(self):
+        new_tag = Tag.objects.create(
+            title=self.cleaned_data['title'],
+            slug=self.cleaned_data['slug']
+        )
+        return new_tag
