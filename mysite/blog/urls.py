2b17943fb59b6d8aace3fef3239b584f1436e5ea
edodo1337 <eldar100@Mail.ru>
2019-03-07 20:46:05 +0300

    7march

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -3,7 +3,9 @@ from .views import *
 
 urlpatterns = [
     path('', posts_list, name='post_list_url'),
+    path('post/<str:slug>/create/', PostCreate.as_view(), name='post_create_url'),
     path('post/<str:slug>/', PostDetail.as_view(), name='post_detail_url' ),
     path('tags/', tags_list, name='tags_list_url'),
+    path('tags/create', TagCreate.as_view(), name='tag_create_url'),
     path('tags/<str:slug>/', TagDetail.as_view(), name='tag_detail_url')
 ]
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import View
 from .models import Post, Tag
 from .utils import ObjectDetailMixin
-
+from .forms import TagForm, PostForm
 
 class PostDetail(ObjectDetailMixin, View):
     model = Post
@@ -12,6 +12,14 @@ class PostDetail(ObjectDetailMixin, View):
 class TagDetail(ObjectDetailMixin, View):
     model = Tag
     template = 'blog/tag_detail.html'
+
+class PostCreate(View):
+    def get(self, request):
+        form = PostForm()
+        return render(request, 'blog/post_create_form.html', context={'posts': posts})
+
+    def post(self, request):
+        bound_form = Post.Forms(request.POST)
 
 
 def posts_list(request):
@@ -29,3 +37,16 @@ def tags_list(request):
 #def tags_detail(request, slug):
 #    tag = Tag.objects.get(slug__iexact=slug)
 #    return render(request,'blog/tags_detail.html', context={'tag': tag})
+
+class TagCreate(View):
+    def get(self, request):
+        form = TagForm()
+        return render(request, 'blog/tag_create.html', context={'form': form})
+
+    def post(self, request):
+        bound_form = TagForm(request.POST)
+
+        if bound_form.is_valid():
+            new_tag = bound_form.save()
+            return redirect(new_tag)
+        return render(request, 'blog/tag_create.html', context={'form': bound_form})
