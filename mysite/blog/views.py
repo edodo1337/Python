06d63e4de115c85186eb98f41a8c0f3772f2b922
edodo1337 <eldar100@Mail.ru>
2019-03-09 22:09:09 +0300

    9march

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -17,24 +17,21 @@ class PostCreate(ObjectCreateMixin, View):
     model_form = PostForm
     template = 'blog/post_create_form.html'
 
+
+class PostUpdate(ObjectUpdateMixin, View):
+    model = Post
+    model_form = PostForm
+    template = 'blog/post_update_form.html'
+
 class TagCreate(ObjectCreateMixin, View):
     model_form = TagForm
     template = 'blog/tag_create.html'
 
-class TagUpdate(View):
-    def get(self, request, slug):
-        tag = Tag.objects.get(slug__iexact=slug)
-        bound_form = TagForm(instance=tag)
-        return render(request, 'blog/tag_update_form.html', context={'form': bound_form, 'tag':tag})
+class TagUpdate(ObjectUpdateMixin, View):
+    model = Tag
+    model_form = TagForm
+    template = 'blog/tag_update_form.html'
 
-    def post(self, request, slug):
-        tag = Tag.objects.get(slug__iexact=slug)
-        bound_form = TagForm(request.POST, instance=tag)
-
-        if bound_form.is_valid():
-            new_tag = bound_form.save()
-            return redirect(new_tag)
-        return render(request, 'blog/tag_update_form', context={'form': bound_form, 'tag':tag})
 
 def posts_list(request):
     posts = Post.objects.all()
